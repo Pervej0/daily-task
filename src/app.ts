6509abed381b25681d990183902fa5f9c1c4dc9f
pervej0 <mdparvez222khan@gmail.com>
2025-02-04 21:30:08 +0600

    basic app setup

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+import cookieParser from "cookie-parser";
+import { notFound } from "./middleware/notFound";
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    message: "Welcome to Server Site",
+  });
+});
+
+app.use(notFound);
+export default app;
