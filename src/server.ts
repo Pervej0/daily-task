402f53c4cf22fc5f0b855ef2ddc38b68f901c0d2
pervej0 <mdparvez222khan@gmail.com>
2025-02-04 21:23:05 +0600

    project server start

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import { Server } from "http";
+import express from "express";
+const PORT = 5000;
+const app = express();
+
+let server: Server;
+const main = async () => {
+  server = app.listen(PORT, () =>
+    console.log("The Server is running on port", PORT)
+  );
+};
+
+// unhandledRejection and uncaughtException error handling--
+process.on("unhandledRejection", () => {
+  console.log("unhandledRejection error detected, server is shutting down!");
+
+  if (server) {
+    server.close();
+    process.exit(1);
+  } else {
+    process.exit(1);
+  }
+});
+
+process.on("uncaughtException", () => {
+  console.log("UncaughtException error detected, server is shutting down!");
+  process.exit(1);
+});
+
+main();
